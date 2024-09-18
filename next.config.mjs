cabaea739b0081b029bce40778a814496ce808c5
James Brooks <kerspoon@gmail.com>
2024-09-18 16:41:43 +0100

    gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    assetPrefix: '/meal-planner/',
+    basePath: '/meal-planner',
 };
 
 export default nextConfig;
